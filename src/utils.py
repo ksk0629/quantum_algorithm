6646a6705511b336012c15c25b5f4bd800832e9f
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2025-02-13 13:48:25 -0500

    Add get_unitary function

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import random
 
 import numpy as np
 import qiskit
+import qiskit_aer
 
 
 def fix_seed(seed: int = 901):
@@ -20,6 +21,22 @@ def get_statevector(
     """Get the statevector corresponding to the given circuit.
 
     :param qiskit.QuantumCircuit circuit: target circuit
-    :return qiskit.quantum_info.states.statevector.Statevector: statevector of the given circuit
+    :return qiskit.quantum_info.states.statevector.Statevector: corresponding statevector
     """
     return qiskit.quantum_info.Statevector(circuit)
+
+
+def get_unitary(
+    circuit: qiskit.QuantumCircuit,
+) -> qiskit_aer.backends.compatibility.Operator:
+    """Get a unitary matrix of the given circuit.
+
+    :param qiskit.QuantumCircuit circuit: target circuit
+    :return qiskit_aer.backends.compatibility.Operator: corresponding unitary
+    """
+    copy_circuit = circuit.copy()
+    copy_circuit.save_unitary()
+    simulator = qiskit_aer.Aer.get_backend("aer_simulator")
+    copy_circuit = qiskit.transpile(copy_circuit, simulator)
+    result = simulator.run(copy_circuit).result()
+    return result.get_unitary(copy_circuit)
